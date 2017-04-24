8278e2cb9ae4eecda679860afd1133d587fdd81a
tc949 <tc949561391@hotmai.com>
2017-04-24 17:42:03 +0800

    fix bug

--- index.js
+++ index.js
@@ -3,15 +3,16 @@
  */
 
 const rd = require('rd');
-modules.export = function routeloader(app, path) {
+module.exports = function routeloader(app, path) {
     return new Promise(function (resolve, reject) {
         rd.each(path, function (f, s, next) {
             if (s.isFile() && f.lastIndexOf('.js') + 3 === f.length) {
                 try {
                     let route = require(f);
                     app.use(route.routes()).use(route.allowedMethods());
+                    console.info(`moondust-koa2-route-loader#the route(${__filename}) load success`);
                 } catch (e) {
-                    console.log(e.message);
+                    console.error(`moondust-koa2-route-loader#the route(${__filename}) cant load because ${e.message}`);
                 }
             }
             next();
